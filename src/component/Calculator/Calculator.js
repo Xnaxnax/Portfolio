2d9d9038e62764221f3061cb16b47894659362c7
Mikhail <ialexei41@gmail.com>
2022-05-09 20:36:00 +0300

    v 1.2.1

--- src/component/Calculator/Calculator.js
+++ src/component/Calculator/Calculator.js
@@ -92,39 +92,42 @@ function Calculator() {
 
   return (
     <div className='calculator'>
-        <div className="wrapper">
-            <div className='screen'>{input !== "" || input === "0" ? 
-              <NumberFormat 
-              value={input} 
-              displayType={'text'} 
-              thousandSeparator={true}/> : 
-              
-              <NumberFormat 
-              value={preState} 
-              displayType={'text'} 
-              thousandSeparator={true}/>}
-            
-            </div>
-            <div className="btn light-gray" onClick={reset}>AC</div>
-            <div className="btn light-gray" onClick={percent}>%</div>
-            <div className="btn light-gray" onClick={minusPlus}>+/-</div>
-            <div className="btn orange" onClick={operatorType}>/</div>
-            <div className="btn" onClick={inputNum}>7</div>
-            <div className="btn" onClick={inputNum}>8</div>
-            <div className="btn" onClick={inputNum}>9</div>
-            <div className="btn orange" onClick={operatorType}>X</div>
-            <div className="btn" onClick={inputNum}>4</div>
-            <div className="btn" onClick={inputNum}>5</div>
-            <div className="btn" onClick={inputNum}>6</div>
-            <div className="btn orange" onClick={operatorType}>+</div>
-            <div className="btn" onClick={inputNum}>1</div>
-            <div className="btn" onClick={inputNum}>2</div>
-            <div className="btn" onClick={inputNum}>3</div>
-            <div className="btn orange" onClick={operatorType}>-</div>
-            <div className="btn zero" onClick={inputNum}>0</div>
-            <div className="btn" onClick={inputNum}>.</div>
-            <div className="btn" onClick={equals}>=</div>
+      <div className="wrapper">
+        <div className='screen'>{input !== "" || input === "0" ? 
+          <NumberFormat 
+          value={input} 
+          displayType={'text'} 
+          thousandSeparator={true}/> : 
+          
+          <NumberFormat 
+          value={preState} 
+          displayType={'text'} 
+          thousandSeparator={true}/>}
+        
         </div>
+        <div className="btn light-gray" onClick={reset}>AC</div>
+        <div className="btn light-gray" onClick={percent}>%</div>
+        <div className="btn light-gray" onClick={minusPlus}>+/-</div>
+        <div className="btn orange" onClick={operatorType}>/</div>
+        <div className="btn" onClick={inputNum}>7</div>
+        <div className="btn" onClick={inputNum}>8</div>
+        <div className="btn" onClick={inputNum}>9</div>
+        <div className="btn orange" onClick={operatorType}>X</div>
+        <div className="btn" onClick={inputNum}>4</div>
+        <div className="btn" onClick={inputNum}>5</div>
+        <div className="btn" onClick={inputNum}>6</div>
+        <div className="btn orange" onClick={operatorType}>+</div>
+        <div className="btn" onClick={inputNum}>1</div>
+        <div className="btn" onClick={inputNum}>2</div>
+        <div className="btn" onClick={inputNum}>3</div>
+        <div className="btn orange" onClick={operatorType}>-</div>
+        <div className="btn zero" onClick={inputNum}>0</div>
+        <div className="btn" onClick={inputNum}>.</div>
+        <div className="btn" onClick={equals}>=</div>
+      </div>
+      <div>
+
+      </div>
     </div>
   )
 }
---
+++ src/component/Weather/Weather.js
@@ -0,0 +1,114 @@
+import React from 'react'
+
+function Weather() {
+    
+  return (
+    <div>
+        <div>
+            <div>
+                <div>
+                    <div>
+                        <span>20°</span>
+                        <p>Сегодня</p>
+                    </div>
+                    <div>
+                        <img src="" alt="" />
+                    </div>
+                </div>
+                <div>
+                    <p>Время: 21:54</p>
+                    <p>Город: Санкт-Петербург</p>
+                </div>
+            </div>
+            <div>
+                <div>
+                    <img src="" alt="" />
+                    <p>Температура</p>
+                    <p>20° - ощущается как 17°</p>
+                </div>
+                <div>
+                    <img src="" alt="" />
+                    <p>Температура</p>
+                    <p>20° - ощущается как 17°</p>
+                </div>
+                <div>
+                    <img src="" alt="" />
+                    <p>Температура</p>
+                    <p>20° - ощущается как 17°</p>
+                </div>
+                <div>
+                    <img src="" alt="" />
+                    <p>Температура</p>
+                    <p>20° - ощущается как 17°</p>
+                </div>
+            </div>
+        </div>
+        <div>
+            <button></button>
+            <button></button>
+            <button></button>
+        </div>
+        <div>
+            <div>
+                <h3>Сегодня</h3>
+                <p>28 авг</p>
+                <img src="" alt="" />
+                <p>+18°</p>
+                <p>+15°</p>
+                <p>Облачно</p>
+            </div>
+            <div>
+                <h3>Сегодня</h3>
+                <p>28 авг</p>
+                <img src="" alt="" />
+                <p>+18°</p>
+                <p>+15°</p>
+                <p>Облачно</p>
+            </div>
+            <div>
+                <h3>Сегодня</h3>
+                <p>28 авг</p>
+                <img src="" alt="" />
+                <p>+18°</p>
+                <p>+15°</p>
+                <p>Облачно</p>
+            </div>
+            <div>
+                <h3>Сегодня</h3>
+                <p>28 авг</p>
+                <img src="" alt="" />
+                <p>+18°</p>
+                <p>+15°</p>
+                <p>Облачно</p>
+            </div>
+            <div>
+                <h3>Сегодня</h3>
+                <p>28 авг</p>
+                <img src="" alt="" />
+                <p>+18°</p>
+                <p>+15°</p>
+                <p>Облачно</p>
+            </div>
+            <div>
+                <h3>Сегодня</h3>
+                <p>28 авг</p>
+                <img src="" alt="" />
+                <p>+18°</p>
+                <p>+15°</p>
+                <p>Облачно</p>
+            </div>
+            <div>
+                <h3>Сегодня</h3>
+                <p>28 авг</p>
+                <img src="" alt="" />
+                <p>+18°</p>
+                <p>+15°</p>
+                <p>Облачно</p>
+            </div>
+        </div>
+
+    </div>
+  )
+}
+
+export default Weather
